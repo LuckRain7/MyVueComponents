2b0de3d1e3e45dff202d24348bf068ab3441fbd6
LuckRain <763839938@qq.com>
2020-03-23 22:17:25 +0800

    add  ChooseCard

---
+++ src/views/ChooseCard/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-row :gutter="20">
+    <!--  -->
+    <ChooseCard></ChooseCard>
+    <!--  -->
+
+    <div class="Sidebar-state">
+      <div class="title">选项卡组件：</div>
+      <div style="height:20px;"></div>
+      <li>请参考传入数据结构<strong>public/data/Menus.json</strong></li>
+    </div>
+    <!--  -->
+  </el-row>
+</template>
+
+<script>
+import ChooseCard from './ChooseCard.vue'
+export default {
+  components: {
+    ChooseCard,
+  },
+}
+</script>
