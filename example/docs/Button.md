1c70bf8e9cdbf857c5567f1d70e4a1911ad68e5f
LuckRain <763839938@qq.com>
2020-05-14 14:54:08 +0800

    feat: add button

---
+++ example/docs/Button.md
@@ -0,0 +1,16 @@
+# Button 按钮
+
+## 基础用法
+
+:::demo
+
+<div class="app-item">
+<l-button>默认</l-button>
+<l-button type="primary">主要按钮</l-button>
+<l-button type="success">成功按钮</l-button>
+<l-button type="warning">信息按钮</l-button>
+<l-button type="danger">警告按钮</l-button>
+<l-button type="text">文本按钮</l-button>
+</div>
+
+:::
--- packages/Button/src/button.vue
+++ packages/Button/src/button.vue
@@ -4,12 +4,34 @@
  * @Date: 2020-05-03 17:47:23
  -->
 <template>
-  <div class="M_button">
-    button按钮组件
-  </div>
+  <button class="l-button" :class="[type ? 'l-button--' + type : '']">
+    <slot></slot>
+  </button>
 </template>
 <script>
 export default {
-  name: "LButton", // 定义这个组件的名称
+  name: 'LButton', // 定义这个组件的名称
+  props: {
+    // 按钮类型
+    type: {
+      type: String,
+      default: 'default'
+    },
+    size: String,
+    icon: {
+      type: String,
+      default: ''
+    },
+    nativeType: {
+      type: String,
+      default: 'button'
+    },
+    loading: Boolean,
+    disabled: Boolean,
+    plain: Boolean,
+    autofocus: Boolean,
+    round: Boolean,
+    circle: Boolean
+  }
 };
 </script>
