79bfcb875492e5f79e79f728c48b21146d88edeb
LuckRain <763839938@qq.com>
2020-05-14 15:06:29 +0800

    update webpack config

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -5,16 +5,12 @@
  */
 
 const path = require('path');
-// 合并配置文件
-const merge = require('webpack-merge');
-const common = require('./webpack.base.js');
+const merge = require('webpack-merge'); // 合并配置文件
+const common = require('./webpack.base.conf.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // 打包之前清除文件
 
 module.exports = merge(common, {
-  module: {},
-  plugins: [],
   mode: 'production',
-  output: {
-    filename: 'js/[name].[contenthash].js', //contenthash 若文件内容无变化，则contenthash 名称不变
-    path: path.resolve(__dirname, '../dist')
-  }
+  plugins: [new CleanWebpackPlugin()],
+  devtool: '#source-map'
 });
