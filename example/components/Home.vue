3a9d3dcfd73d60058eb11e6451a65818e42ddf82
LuckRain <763839938@qq.com>
2020-05-15 11:54:03 +0800

    feat:add home main componemnts

--- example/components/Home.vue
+++ example/components/Home.vue
@@ -4,13 +4,104 @@
  * @Date: 2020-05-14 13:19:26
  -->
 <template>
-  <div>
-    hello world
+  <div class="home">
+    <!-- logo title -->
+    <div class="home-logo">
+      <div class="title">
+        <h1>LuckRain7 制作的 Vue 组件库</h1>
+        <h2>收录一些自己制作的 Vue 组件</h2>
+      </div>
+      <img src="../assets/home.svg" alt="" />
+    </div>
+    <!-- 介绍 -->
+    <div class="description">
+      <template v-for="(item, index) in descriptionData">
+        <home-card :key="index" :data="item"></home-card>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import HomeCard from './HomeCard.vue';
+export default {
+  name: 'Home',
+  data() {
+    return {
+      descriptionData: [
+        {
+          title: 'Guide',
+          sub: '指南',
+          description: '了解项目的建立初衷、目标、如何安装等'
+        },
+        {
+          title: 'Components',
+          sub: '组件',
+          description: '组件 Demo 及说明文档'
+        },
+        {
+          title: 'Theme',
+          sub: '主题',
+          description: '在线主题编辑'
+        },
+        {
+          title: 'Source',
+          sub: '资源',
+          description: '项目设计资源及参考资源'
+        }
+      ]
+    };
+  },
+  components: {
+    HomeCard
+  }
+};
 </script>
 
-<style></style>
+<style lang="scss">
+.home {
+  // logo
+  .home-logo {
+    width: 890px;
+    margin: 0 auto;
+    text-align: center;
+    // 标题
+    .title {
+      margin: 50px auto;
+      h1 {
+        font-size: 34px;
+        margin: 0;
+        line-height: 48px;
+        color: #555;
+      }
+      h2 {
+        font-size: 18px;
+        line-height: 28px;
+        color: #888;
+        margin: 10px 0 5px;
+      }
+    }
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  .description {
+    margin: 50px auto;
+    width: 100%;
+    height: 430px;
+    display: flex;
+    align-items: stretch;
+    justify-content: space-between;
+    .des-item {
+      border: 1px solid #eaeefb;
+      border-radius: 5px;
+      box-sizing: border-box;
+      height: 100%;
+      width: 252.5px;
+    }
+  }
+}
+</style>
---
+++ example/components/HomeCard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="home-card">
+    <div class="card-imgages">
+      {{ data.title }}
+    </div>
+    <div class="card-title">{{ data.sub }}</div>
+    <div class="card-description">{{ data.description }}</div>
+    <div class="link-go">查看详情</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object
+  }
+};
+</script>
+
+<style lang="scss">
+.home-card {
+  border: 1px solid #eaeefb;
+  border-radius: 5px;
+  box-sizing: border-box;
+  height: 100%;
+  width: 252.5px;
+  text-align: center;
+  .card-imgages {
+    height: 160px;
+    width: 100%;
+    margin: 30px auto;
+    line-height: 160px;
+    text-align: center;
+    font-size: 30px;
+    color: rgb(2, 171, 233);
+  }
+  .card-title {
+    margin: 0;
+    font-size: 18px;
+    color: #1f2f3d;
+    font-weight: 400;
+    margin: 10px auto;
+  }
+  .card-description {
+    height: 65px;
+    font-size: 14px;
+    color: #99a9bf;
+    padding: 0 25px;
+    line-height: 20px;
+    margin: 10px auto;
+  }
+  .link-go {
+    margin-top: 55px;
+    height: 50px;
+    line-height: 50px;
+    font-size: 14px;
+    color: #409eff;
+    border: 0;
+    border-top: 1px solid #eaeefb;
+    cursor: pointer;
+    width: 100%;
+    background-color: #fff;
+    border-radius: 0 0 5px 5px;
+    transition: all 0.3s;
+    text-decoration: none;
+    display: block;
+  }
+  transition: all 0.3s ease-in-out;
+}
+
+.home-card:hover {
+  box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);
+  margin-top: -10px;
+  transition: all 0.3s ease-in-out;
+}
+</style>
